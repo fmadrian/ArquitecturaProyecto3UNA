17f8b3aa5be0614b76a387fdb5604243bba551b9
Adrián <47431198+fmadrian@users.noreply.github.com>
2024-11-12 17:25:15 -0600

    Páginas de batallas, muertes, personajes y casas añadidas

---
+++ pages/battles/[name].vue
@@ -0,0 +1,17 @@
+<!-- pages/director/[...id].vue -->
+<template>
+    <div class="container">
+        <HeaderView />
+        <div class="row py-4 gx-4">
+            {{ item }}
+        </div>
+        <FooterView />
+    </div>
+</template>
+<script setup>
+const route = useRoute()
+
+const item = (await queryContent('/battles').only('body').findOne()).body
+    .find(item => item.name.trim() === route.params.name.trim())
+
+</script>
---
+++ pages/houses/index.vue
@@ -0,0 +1,13 @@
+<!-- pages/authors_index.vue -->
+<template>
+    <div class="container">
+        <HeaderView />
+        <div>
+            {{ items }}
+        </div>
+        <FooterView />
+    </div>
+</template>
+<script setup>
+const items = (await queryContent('/houses').only('body').findOne()).body;
+</script>
