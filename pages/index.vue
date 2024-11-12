f331e3ea91f32ba6641b2e3266057a1b21bac72a
Adrián <47431198+fmadrian@users.noreply.github.com>
2024-11-12 15:11:56 -0600

    Initial commit

---
+++ pages/index.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="container">
+        <div class="d-flex flex-column row-gap-4">
+            <HeaderView />
+
+            <div class="d-flex flex-column row-gap-2">
+                <h1>Proyecto 3</h1>
+                <h3>Arquitectura de información</h3>
+                <div class="d-flex flex-column">
+                    <h5>Fernanda Quesada</h5>
+                    <h5>Adrián Fallas</h5>
+                </div>
+            </div>
+
+            <FooterView />
+        </div>
+    </div>
+</template>
+<script setup>
+
+</script>
