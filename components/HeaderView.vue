6b8208bc6e5cf1a6877e49850aa85e7a9d839301
Adrián <47431198+fmadrian@users.noreply.github.com>
2024-11-13 22:23:16 -0600

    Nuevos botones en encabezado

--- components/HeaderView.vue
+++ components/HeaderView.vue
@@ -2,11 +2,11 @@
     <div class="d-flex flex-row column-gap-3" style="margin-top: 15px">
         <NuxtLink class="col-span-3 btn btn-outline-dark" to="/">
             Proyecto 3</NuxtLink>
-        <NuxtLink class="col-span-3 btn btn-outline-dark" to="/pelicula">
-            Películas</NuxtLink>
-        <NuxtLink class="col-span-3 btn btn-outline-dark" to="/director">Directores</NuxtLink>
-        <NuxtLink class="col-span-3 btn btn-outline-dark" to="/estudio">
-            Estudios</NuxtLink>
-
+        <NuxtLink class="col-span-3 btn btn-outline-dark" to="/battles">Battallas</NuxtLink>
+        <NuxtLink class="col-span-3 btn btn-outline-dark" to="/characters">Personajes</NuxtLink>
+        <NuxtLink class="col-span-3 btn btn-outline-dark" to="/deaths">Muertes</NuxtLink>
+        <NuxtLink class="col-span-3 btn btn-outline-dark" to="/houses">Casas</NuxtLink>
+        <NuxtLink class="col-span-3 btn btn-outline-dark" to="/locations">Ubicaciones</NuxtLink>
     </div>
+    <hr>
 </template>
