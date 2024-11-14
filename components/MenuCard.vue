1f9b93f9435acb9975526b56cb50e46e364c8e06
Adrián <47431198+fmadrian@users.noreply.github.com>
2024-11-13 23:11:53 -0600

    Primer menú principal

---
+++ components/MenuCard.vue
@@ -0,0 +1,29 @@
+<template>
+    <NuxtLink class="text-decoration-none" :to=to>
+        <div class="card ">
+            <img class="card-img-top" :src=src :alt=title>
+            <div class="card-body">
+                <h5 class="card-title">{{ title }}</h5>
+            </div>
+        </div>
+    </NuxtLink>
+</template>
+
+<script setup>
+
+const props = defineProps({
+    title: {
+        type: String,
+        required: true
+    },
+    src: {
+        type: String,
+        required: true
+    },
+    to: {
+        type: String,
+        required: true
+    }
+})
+
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -12,6 +12,21 @@
                 </div>
             </div>
 
+            <div class="d-flex flex-row column-gap-2 row-gap-4 align-items-center justify-content-around  flex-wrap">
+                <MenuCard class="col-3" title="Casas"
+                    src="https://images.vexels.com/content/78080/preview/game-of-thrones-houses-03e8d1.png"
+                    to="/houses" />
+                <MenuCard class="col-3" title="Batallas"
+                    src="https://static1.colliderimages.com/wordpress/wp-content/uploads/2023/06/gotbattlemulti.jpg"
+                    to="/battles" />
+                <MenuCard class="col-3" title="Muertes" src="https://i.imgflip.com/4s4jwa.png?a480648" to="/deaths" />
+                <MenuCard class="col-3" title="Ubicaciones"
+                    src="https://www.expedia.com/stories/wp-content/uploads/2021/09/dragonstone-1.jpg"
+                    to="/locations" />
+                <MenuCard class="col-3" title="Personajes"
+                    src="https://i0.wp.com/the-avocado.org/wp-content/uploads/2019/05/n.jpg" to="/characters" />
+            </div>
+
             <FooterView />
         </div>
     </div>
@@ -19,3 +34,5 @@
 <script setup>
 
 </script>
+
+<!--https://png.pngtree.com/background/20230519/original/pngtree-stairway-leading-up-to-heaven-in-the-clouds-picture-image_2665145.jpg-->
